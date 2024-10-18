e36e4e699a40b09b9d9e9e5258b0663a878fe85f
Viykym <kimanivienna1@gmail.com>
2024-10-18 08:26:04 +0300

    Add files via upload

---
+++ Array 2D.c
@@ -0,0 +1,20 @@
+//Arrays in C-2D arrays
+/*
+program for 2D arrys
+Auther:Brian muriuki joseph
+Reg No:CT102/G/22804/24
+Date:17/10/2024
+*/
+//preprocessor directive-printf(),scanf() 
+#include<stdio.h>
+int main (){
+	int i,j;
+	int marks[2][3]={{50,90,60},{40,70,40}};
+	for(i=0;i<2;i++){
+		for(j=0;j<3;j++){
+			printf("marks[%d][%d]=%d\n",i,j, marks[i][j]);
+			}
+			
+}
+	return 0;
+}
